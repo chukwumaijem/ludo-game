1873b4f003a881076b423a9ad2727cc3990632af
Chukwuma Ezumezu <chukwuma.ezumezu@gmail.com>
2018-05-05 04:17:43 +0100

    use constants for seed settings

--- src/reducers/gameData.js
+++ src/reducers/gameData.js
@@ -1,34 +1,35 @@
 import Types from '../actions/actionTypes';
 import { setColours } from '../utils/moveSeed';
+import { still, movesLeft } from '../utils/constants';
 
-// movesLeft here is minimum number of moves needed to finish after coming out of the house.
+// movesLeft here is the minimum number of moves needed to finish after coming out of the house.
 const initialState = {
   houseOneCards: {
-    'H1-C1': { position: 'still', movesLeft: 56 },
-    'H1-C2': { position: 'still', movesLeft: 56 },
-    'H1-C3': { position: 'still', movesLeft: 56 },
-    'H1-C4': { position: 'still', movesLeft: 56 },
+    'H1-C1': { position: still, movesLeft: movesLeft },
+    'H1-C2': { position: still, movesLeft: movesLeft },
+    'H1-C3': { position: still, movesLeft: movesLeft },
+    'H1-C4': { position: still, movesLeft: movesLeft },
     'H1-Colour': 'blue'
   },
   houseTwoCards: {
-    'H2-C1': { position: 'still', movesLeft: 56 },
-    'H2-C2': { position: 'still', movesLeft: 56 },
-    'H2-C3': { position: 'still', movesLeft: 56 },
-    'H2-C4': { position: 'still', movesLeft: 56 },
+    'H2-C1': { position: still, movesLeft: movesLeft },
+    'H2-C2': { position: still, movesLeft: movesLeft },
+    'H2-C3': { position: still, movesLeft: movesLeft },
+    'H2-C4': { position: still, movesLeft: movesLeft },
     'H2-Colour': 'red'
   },
   houseThreeCards: {
-    'H3-C1': { position: 'still', movesLeft: 56 },
-    'H3-C2': { position: 'still', movesLeft: 56 },
-    'H3-C3': { position: 'still', movesLeft: 56 },
-    'H3-C4': { position: 'still', movesLeft: 56 },
+    'H3-C1': { position: still, movesLeft: movesLeft },
+    'H3-C2': { position: still, movesLeft: movesLeft },
+    'H3-C3': { position: still, movesLeft: movesLeft },
+    'H3-C4': { position: still, movesLeft: movesLeft },
     'H3-Colour': 'yellow'
   },
   houseFourCards: {
-    'H4-C1': { position: 'still', movesLeft: 56 },
-    'H4-C2': { position: 'still', movesLeft: 56 },
-    'H4-C3': { position: 'still', movesLeft: 56 },
-    'H4-C4': { position: 'still', movesLeft: 56 },
+    'H4-C1': { position: still, movesLeft: movesLeft },
+    'H4-C2': { position: still, movesLeft: movesLeft },
+    'H4-C3': { position: still, movesLeft: movesLeft },
+    'H4-C4': { position: still, movesLeft: movesLeft },
     'H4-Colour': 'green'
   },
   playerTurn: 'P1',
