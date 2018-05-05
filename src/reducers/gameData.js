246b3ba86d27c94fe5af31d6dc5a2c83de487f7f
Chukwuma Ezumezu <chukwuma.ezumezu@gmail.com>
2018-05-05 04:09:41 +0100

    change game data structure

--- src/reducers/gameData.js
+++ src/reducers/gameData.js
@@ -1,34 +1,35 @@
 import Types from '../actions/actionTypes';
 import { setColours } from '../utils/moveSeed';
 
+// movesLeft here is minimum number of moves needed to finish after coming out of the house.
 const initialState = {
   houseOneCards: {
-    'H1-C1': 'HL-01',
-    'H1-C2': 'still',
-    'H1-C3': 'still',
-    'H1-C4': 'still',
-    'H1-Colour': null
+    'H1-C1': { position: 'HL-01', movesLeft: 56 },
+    'H1-C2': { position: 'still', movesLeft: 56 },
+    'H1-C3': { position: 'HL-14', movesLeft: 2 },
+    'H1-C4': { position: 'still', movesLeft: 56 },
+    'H1-Colour': 'blue'
   },
   houseTwoCards: {
-    'H2-C1': 'VT-12',
-    'H2-C2': 'still',
-    'H2-C3': 'still',
-    'H2-C4': 'still',
-    'H2-Colour': null
+    'H2-C1': { position: 'VT-12', movesLeft: 56 },
+    'H2-C2': { position: 'still', movesLeft: 56 },
+    'H2-C3': { position: 'VT-41', movesLeft: 2 },
+    'H2-C4': { position: 'still', movesLeft: 56 },
+    'H2-Colour': 'red'
   },
   houseThreeCards: {
-    'H3-C1': 'VB-40',
-    'H3-C2': 'still',
-    'H3-C3': 'still',
-    'H3-C4': 'still',
-    'H3-Colour': null
+    'H3-C1': { position: 'VB-40', movesLeft: 56 },
+    'H3-C2': { position: 'still', movesLeft: 56 },
+    'H3-C3': { position: 'VB-11', movesLeft: 2 },
+    'H3-C4': { position: 'still', movesLeft: 56 },
+    'H3-Colour': 'yellow'
   },
   houseFourCards: {
-    'H4-C1': 'HR-24',
-    'H4-C2': 'still',
-    'H4-C3': 'still',
-    'H4-C4': 'still',
-    'H4-Colour': null
+    'H4-C1': { position: 'HR-24', movesLeft: 56 },
+    'H4-C2': { position: 'still', movesLeft: 56 },
+    'H4-C3': { position: 'HR-11', movesLeft: 2 },
+    'H4-C4': { position: 'still', movesLeft: 56 },
+    'H4-Colour': 'green'
   },
   playerTurn: 'P1',
 };
