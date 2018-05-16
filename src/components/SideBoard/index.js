259ae1f3731040efe28b61868c7665131975eaf5
Chukwuma Ezumezu <chukwuma.ezumezu@gmail.com>
2018-05-16 14:58:59 +0100

    add die roll to side bar display die roll result

--- src/components/SideBoard/index.js
+++ src/components/SideBoard/index.js
@@ -1,17 +1,70 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import rollADie from 'roll-a-die';
+import shortId from 'shortid';
 
 import { moveSeedToPosition } from '../../actions';
+import GameChat from '../GameChat';
 
 import styles from './SideBoard.module.css';
 
 class SideBoard extends Component {
-  setSeed = () => {
-    const seedId = document.getElementById('seedId').value;
-    const moves = document.getElementById('seedPosition').value;
-    this.props.moveSeedToPosition(seedId, moves.split(''));
+  state = {
+    element: null,
+    results: [],
   }
+
+  componentDidMount() {
+    const rollDieElement = document.getElementById('roll-die');
+    this.setState({
+      element: rollDieElement,
+    });
+  }
+
+  setDieRollResult = (results) => {
+    const resultObjects = results.map(result => {
+      return { id: shortId.generate(), value: result, selected: false };
+    });
+    this.setState(prevState => {
+      return {
+        results: [...prevState.results, ...resultObjects]
+      }
+    })
+  }
+
+  rollDice = () => {
+    const options = {
+      element: this.state.element,
+      numberOfDice: 2,
+      callback: this.setDieRollResult
+    };
+    rollADie(options);
+  }
+
+  toggleDieResultSelected = (id) => {
+    const results = this.state.results.map(result => {
+      if (result.id === id) {
+        result.selected = !result.selected;
+      }
+      return result;
+    });
+    this.setState({ results });
+  }
+
+  renderDieResults = () => {
+    const results = this.state.results;
+    return results.map(result =>
+      <div
+        key={result.id}
+        className={`${styles.dieResult} ${result.selected ? styles.selectedResult : ''}`}
+        onClick={() => this.toggleDieResultSelected(result.id)}
+      >
+        {result.value}
+      </div>
+    )
+  }
+
   render() {
     const sideBoardWidth = this.props.sideBoardWidth;
     const containerStyle = {
@@ -20,18 +73,27 @@ class SideBoard extends Component {
       marginLeft: sideBoardWidth * 0.1,
       position: 'relative',
       height: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center'
     };
+
     return (
       <div style={containerStyle}>
-        <div className={styles.chatWindowContainer}>
-          Chat Window
-        </div>
+        <hr className={styles.hRule} />
+        <GameChat />
+        <hr className={styles.hRule} />
         <div className={styles.dieWindowContainer}>
-          <div>Roll</div>
           <div>
-            Roll Results
+            {this.renderDieResults()}
+          </div>
+          <div id="roll-die"></div>
+          <div className={styles.rollDieButton} onClick={this.rollDice}>
+            Roll Dice
           </div>
         </div>
+        <hr className={styles.hRule} />
       </div>
     )
   }
