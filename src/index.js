6f9f287576186b648ea48923795fdeb6d2932438
Chukwuma Ezumezu <chukwuma.ezumezu@gmail.com>
2018-05-25 22:45:51 +0100

    refactor game main index file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import GameFrame from './containers/GamePlay';
+import Home from './containers/HomePage';
 import store from './store';
 // import registerServiceWorker from './registerServiceWorker';
 
@@ -10,24 +12,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'toastr/build/toastr.css';
 import './mystyles.css';
 
-class Game extends Component {
-  render() {
-    const winSize = window.innerHeight * 0.95;
-    const gameContainerStyle = {
-      height: winSize,
-      display: 'flex',
-      justifyContent: 'center'
-    };
-    return (
-      <Provider store={store}>
-        <div style={gameContainerStyle}>
-          <div className="game-frame">
-            <GameFrame gameBoardHeight={winSize} />
-          </div>
-        </div>
-      </Provider>
-    );
-  }
+const Game = () => {
+  return (<Provider store={store}>
+    <Router>
+      <div className="game-frame">
+        <Route exact path="/" component={Home} />
+        <Route path="/game" component={GameFrame} />
+      </div>
+    </Router>
+  </Provider>
+  );
 };
 
 render(<Game />, document.getElementById('root'));
