d4bc46312ca32c2d6a7ee9c1feef871b1623196c
Chukwuma Ezumezu <chukwuma.ezumezu@gmail.com>
2018-05-28 12:36:32 +0100

    build page header

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Header from '../../components/Header';
 
 const Home = () => <div>
-  <Header />
   <div>
     <div>Simple Game Description</div>
     <Link to="game">Start Game Action Item</Link>
   </div>
-  <div>Footer</div>
 </div>;
 
 export default Home;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import GameFrame from './containers/GamePlay';
-import Home from './containers/HomePage';
+import HomePage from './containers/HomePage';
+import Header from './components/Header';
+
 import store from './store';
 // import registerServiceWorker from './registerServiceWorker';
 
@@ -12,13 +14,22 @@ import 'semantic-ui-css/semantic.min.css';
 import 'toastr/build/toastr.css';
 import './mystyles.css';
 
+const Home = () => <div>
+  <Header />
+  <Switch>
+    <Route exact path="/" component={HomePage} />
+    <Route path="how-to-play" />
+  </Switch>
+  <div>Footer</div>
+</div>;
+
 const Game = () => {
   return (<Provider store={store}>
     <Router>
-      <div>
-        <Route exact path="/" component={Home} />
-        <Route path="/game" component={GameFrame} />
-      </div>
+      <Switch>
+        <Route path="/play/:roomid" component={GameFrame} />
+        <Route render={() => Home()} />
+      </Switch>
     </Router>
   </Provider>
   );
